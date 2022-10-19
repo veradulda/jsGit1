544350c47bf7a47c5fc463367e08d8817f98f99f
veraDulda <99422897+veraDulda@users.noreply.github.com>
2022-10-19 18:38:17 +0300

    2vv

---
+++ 1-variable.js
@@ -0,0 +1,13 @@
+zzzzvar a,b,c;
+
+a = 10;
+b = 20;
+c = a;
+a = b;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+console.console.log("git deneme");
+console.console.log("git deneme2");
--- 2-LabeledStatements.js
+++ 2-LabeledStatements.js
@@ -1,1 +1,4 @@
+for(var i = 0 ; i < 3 ; i ++){
+  console.log(i + ' in the loop');
+}
 console.log(i + ' out of the loop' );
